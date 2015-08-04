ac3662f4cea3d179b761915df5dbe98796f32f9c
fsamotorcan <fsamot@gmail.com>
2015-08-04 14:56:04 +0200

    Remove old projects.

--- src/Samotorcan.Tests.Windows.Application/Scripts/ControllerConstructorTest.js
+++
@@ -1,5 +0,0 @@
-﻿var app = angular.module('ControllerConstructorTestApp', ['htmlUi']);
-
-app.factory('ControllerConstructorTest', ['htmlUi.controller', function (htmlUiController) {
-    return htmlUiController.createController('ControllerConstructorTest');
-}]);
